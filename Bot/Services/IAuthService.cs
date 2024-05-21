2cd4cfa2e30e8b5d325ebfe61e3d63028877e021
nhachan <chanb2014731@student.ctu.edu.vn>
2024-05-21 16:36:55 +0700

    add username

--- Bot/Services/IAuthService.cs
+++ Bot/Services/IAuthService.cs
@@ -1,12 +1,13 @@
 ﻿using Bot.DTO;
 using Bot.Request;
+using Bot.Response;
 using Microsoft.AspNetCore.Identity;
 
 namespace Bot.Services
 {
     public interface IAuthService
     {
-        Task<TokenModel?> Login(LoginRequest request);
+        Task<JwtResponse?> Login(LoginRequest request);
         Task<IdentityResult> Register(RegisterRequest request);
         Task<TokenModel?> RefreshToken(TokenModel token);
 
